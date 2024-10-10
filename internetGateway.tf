29fd5c307905b7dbaf9d75614613c63b8a558d7c
swamyms <mudiga.s@northeastern.edu>
2024-10-09 23:19:25 -0400

    Terraform fmt applied

--- internetGateway.tf
+++ internetGateway.tf
@@ -1,7 +1,7 @@
 resource "aws_internet_gateway" "gw" {
- vpc_id = aws_vpc.csye6225_Swamy_Dev.id
- 
- tags = {
-   Name = "IG Cloud Project"
- }
+  vpc_id = aws_vpc.csye6225_Swamy_Dev.id
+
+  tags = {
+    Name = "IG Cloud Project"
+  }
 }

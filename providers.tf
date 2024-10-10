757d01d023741697edbcece67ba2a84d5bd49e8f
swamyms <mudiga.s@northeastern.edu>
2024-10-09 23:17:47 -0400

    Added variable for fetching region dynamically

--- providers.tf
+++ providers.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  #default     = "us-east-1" # Optional default value
+}
+
 provider "aws" {
   region = var.region
 }

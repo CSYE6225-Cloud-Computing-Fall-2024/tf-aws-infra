13764bf4dfb11ad21233da35964c05d88d212838
swamyms <mudiga.s@northeastern.edu>
2024-10-10 00:01:34 -0400

    Space formatted

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "csye6225_Swamy_Dev" {
- cidr_block = "10.0.0.0/16"
- 
- tags = {
-   Name = "VPC Cloud Project"
- }
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "VPC Cloud Project"
+  }
 }

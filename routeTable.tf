a1e7367c3118c5292ae3635be22b12bcbb1b4b59
Swamy Mudiga <145133400+swamymudiga@users.noreply.github.com>
2024-10-16 14:35:02 -0400

    Update routeTable.tf

--- routeTable.tf
+++ routeTable.tf
@@ -10,6 +10,7 @@ resource "aws_route_table_association" "public_subnet_associations" {
   count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
 }
 
 # Create route to the internet

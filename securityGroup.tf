efafa7f2560a8ea9aad90721aa49ae6353dcdf0e
swamyms <mudiga.s@northeastern.edu>
2024-10-17 10:53:16 -0400

    Web application security group

---
+++ securityGroup.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "app_security_group" {
+  vpc_id = aws_vpc.csye6225_Swamy_Dev.id
+  name   = "application_security_group"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Application Traffic"
+    from_port   = var.application_port # Using the variable for the application port
+    to_port     = var.application_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "App Security Group"
+  }
+}

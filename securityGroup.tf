7ebc894bd0288aec0d12b28adbe864687488b6f2
swamyms <mudiga.s@northeastern.edu>
2024-10-22 20:03:20 -0400

    Adding Secruity Group for RDS Postgres SQL

--- securityGroup.tf
+++ securityGroup.tf
@@ -45,3 +45,29 @@ resource "aws_security_group" "app_security_group" {
     Name = "App Security Group"
   }
 }
+
+
+# Security group for RDS instance
+resource "aws_security_group" "db_security_group" {
+  vpc_id = aws_vpc.csye6225_Swamy_Dev.id
+  name   = "database_security_group"
+
+  ingress {
+    description     = "Allow PostgresSQL access from application security group"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_security_group.id] # Only allow from app SG
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "DB Security Group"
+  }
+}

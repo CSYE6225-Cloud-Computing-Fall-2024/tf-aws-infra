44efac73f451b2b255e03eb17e4b706dd6b4c3e8
swamyms <mudiga.s@northeastern.edu>
2024-10-23 03:47:04 -0400

    Removed the variable region

--- providers.tf
+++ providers.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  #default     = "us-east-1" # Optional default value
-}
-
 provider "aws" {
   region = var.region
 }

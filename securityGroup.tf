507566fae8dce02ba98185a655608579a83a677a
swamyms <mudiga.s@northeastern.edu>
2024-11-03 07:57:21 -0500

    1. Changed the Ingress rule by adding the load balance security group which tightening the application security group will only accept the request from load balancer only

--- securityGroup.tf
+++ securityGroup.tf
@@ -2,42 +2,29 @@ resource "aws_security_group" "app_security_group" {
   vpc_id = aws_vpc.csye6225_Swamy_Dev.id
   name   = "application_security_group"
 
+  # Allow SSH access only from the load balancer security group
   ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow SSH from load balancer"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_security_group.id] # Restrict to LB security group
   }
 
+  # Allow application traffic only from the load balancer security group
   ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow Application Traffic from load balancer"
+    from_port       = var.application_port # Port defined in variable for flexibility
+    to_port         = var.application_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_security_group.id] # Restrict to LB security group
   }
 
-  ingress {
-    description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow Application Traffic"
-    from_port   = var.application_port # Using the variable for the application port
-    to_port     = var.application_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1" # Allow all outbound traffic
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -69,5 +56,41 @@ resource "aws_security_group" "db_security_group" {
 
   tags = {
     Name = "DB Security Group"
+  }
+}
+
+# Security Group for Load Balancer
+resource "aws_security_group" "lb_security_group" {
+  vpc_id = aws_vpc.csye6225_Swamy_Dev.id
+  name   = "load_balancer_security_group"
+
+  # Allow inbound HTTP traffic on port 80
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow inbound HTTPS traffic on port 443
+  ingress {
+    description = "Allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Load Balancer Security Group"
   }
 }

f41d2da6f5b9e60ea8ae83d2399e917f35781a83
swamyms <mudiga.s@northeastern.edu>
2024-11-06 19:26:03 -0500

    Commented security groups in ingress to Allow SSH on port 22 and CIDR "0.0.0.0/0"

--- securityGroup.tf
+++ securityGroup.tf
@@ -4,11 +4,12 @@ resource "aws_security_group" "app_security_group" {
 
   # Allow SSH access only from the load balancer security group
   ingress {
-    description     = "Allow SSH from load balancer"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb_security_group.id] # Restrict to LB security group
+    description = "Allow SSH from load balancer"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    #security_groups = [aws_security_group.lb_security_group.id] # Restrict to LB security group
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Allow application traffic only from the load balancer security group
